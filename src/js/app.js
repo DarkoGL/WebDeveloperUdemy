683bf8fa4c612fc1e93e43a6571c5f319bf84e82
Adolfo Gomez <agomezligero@yahoo.es>
2022-09-29 15:15:41 +0200

    Proyecto terminado

---
+++ src/js/app.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", () => {
+    iniciarApp();
+});
+
+function iniciarApp(){
+    crearGaleria();
+    scrollNav();
+    navegacionFija();
+}
+
+function navegacionFija(){
+    const barra = document.querySelector("header");
+    const sobreFestival = document.querySelector(".sobre-festival");
+    const body = document.querySelector("body");
+
+    window.addEventListener("scroll", ()=> {
+        console.log(sobreFestival.getBoundingClientRect());
+
+        if(sobreFestival.getBoundingClientRect().bottom < 0){
+            barra.classList.add("fijo");
+            body.classList.add("body-scroll");
+        }else{
+            barra.classList.remove("fijo");
+            body.classList.remove("body-scroll");
+        }
+    })
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll(".navegacion-principal a");
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("click", (e)=>{
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    })
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector(".galeria-imagenes");
+
+    for(let i = 1; i <= 12; i++){
+        const imagen = document.createElement("picture");
+        imagen.innerHTML = `
+                <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+                <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" 
+                src="build/img/thumb/${i}.jpg" alt="Imagen Vocalista">
+        `;
+
+        imagen.onclick = function(){
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(id){
+    const imagen = document.createElement("picture");
+        imagen.innerHTML = `
+                <source srcset="build/img/grande/${id}.avif" type="image/avif">
+                <source srcset="build/img/grande/${id}.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" 
+                src="build/img/grande/${id}.jpg" alt="Imagen galeria">
+        `;
+    //Crea el overlay con la imagen
+    const overlay = document.createElement("DIV");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+    overlay.onclick = () =>{
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body");
+        overlay.remove();
+    }
+
+    //Botón para cerrar el Modal
+    const cerrarImagen = document.createElement("P");
+    cerrarImagen.textContent = "X";
+    cerrarImagen.classList.add("btn-cerrar");
+    cerrarImagen.onclick = () => {
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body");
+        overlay.remove();
+    }
+
+    overlay.appendChild(cerrarImagen);
+
+    //Añadirlo al HTML
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body");
+}
